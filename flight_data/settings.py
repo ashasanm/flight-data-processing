29443ffbbbe8fd39ef9584612adea7a7df1b2fd2
Hasan <anas.hasan@loratechai.com>
2024-11-15 18:33:59 +0700

    remove default dbconn for local dev

--- flight_data/settings.py
+++ flight_data/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-default-secret-key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1,*").split(',')
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1,*").split(",")
 
 # Application definition
 INSTALLED_APPS = [
@@ -61,11 +61,11 @@ WSGI_APPLICATION = "flight_data.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DATABASE_NAME", "flight_data"),
-        "USER": os.getenv("DATABASE_USER", "postgres"),
-        "PASSWORD": os.getenv("DATABASE_PASSWORD", "password"),
-        "HOST": os.getenv("DATABASE_HOST", "psqldb"),
-        "PORT": os.getenv("DATABASE_PORT", "5432"),
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": os.getenv("POSTGRES_HOST"),
+        "PORT": os.getenv("POSTGRES_PORT"),
     }
 }
 
@@ -151,12 +151,22 @@ SPARK_CONFIG_OPTION = {
     "spark.driver.memory": os.getenv("SPARK_DRIVER_MEMORY", "4g"),
     "spark.executor.cores": os.getenv("SPARK_EXECUTOR_CORES", "2"),
     "spark.num.executors": os.getenv("SPARK_NUM_EXECUTORS", "10"),
-    "spark.dynamicAllocation.enabled": os.getenv("SPARK_DYNAMIC_ALLOCATION_ENABLED", "true"),
-    "spark.dynamicAllocation.minExecutors": os.getenv("SPARK_DYNAMIC_ALLOCATION_MIN_EXECUTORS", "1"),
-    "spark.dynamicAllocation.maxExecutors": os.getenv("SPARK_DYNAMIC_ALLOCATION_MAX_EXECUTORS", "50"),
+    "spark.dynamicAllocation.enabled": os.getenv(
+        "SPARK_DYNAMIC_ALLOCATION_ENABLED", "true"
+    ),
+    "spark.dynamicAllocation.minExecutors": os.getenv(
+        "SPARK_DYNAMIC_ALLOCATION_MIN_EXECUTORS", "1"
+    ),
+    "spark.dynamicAllocation.maxExecutors": os.getenv(
+        "SPARK_DYNAMIC_ALLOCATION_MAX_EXECUTORS", "50"
+    ),
     "spark.sql.shuffle.partitions": os.getenv("SPARK_SQL_SHUFFLE_PARTITIONS", "200"),
     "spark.sql.adaptive.enabled": os.getenv("SPARK_SQL_ADAPTIVE_ENABLED", "true"),
-    "spark.sql.adaptive.shuffle.targetPostShuffleInputSize": os.getenv("SPARK_SQL_ADAPTIVE_SHUFFLE_TARGET_SIZE", "134217728"),
+    "spark.sql.adaptive.shuffle.targetPostShuffleInputSize": os.getenv(
+        "SPARK_SQL_ADAPTIVE_SHUFFLE_TARGET_SIZE", "134217728"
+    ),
 }
 
-SPARK_BAD_RECORDS_PATH = os.getenv("SPARK_BAD_RECORDS_PATH", "/flight_data/spark/records")
+SPARK_BAD_RECORDS_PATH = os.getenv(
+    "SPARK_BAD_RECORDS_PATH", "/flight_data/spark/records"
+)
